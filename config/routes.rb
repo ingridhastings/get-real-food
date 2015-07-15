3d6ef30b6decaf8af56a4448197af1045932f675
Ingrid Hastings <ingridhastings@gmail.com>
2015-07-14 23:01:15 -0500

    Finishing touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :farms
+
+  resources :farms, :reviews, :user
+
   resources :reviews
+
   devise_for :users, controllers: { registrations: "registrations"}
+
   root 'pages#home'
+
   get 'dashboard', to: 'pages#dashboard'
 
 end
